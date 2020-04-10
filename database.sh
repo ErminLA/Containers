2573c758be08844fb25c52cbd893467628aa046b
ErminLA <42754356+ErminLA@users.noreply.github.com>
2020-04-10 21:21:11 +0200

    Update database.sh

--- database.sh
+++ database.sh
@@ -1,6 +1,6 @@
 lxc exec Database -- sh -c 'mysql -u root -e "CREATE DATABASE MyDB;"'
-lxc exec Database -- sh -c "mysql -u root -e \"CREATE USER 'chronic' IDENTIFIED BY 'Test321';\""
-lxc exec Database -- sh -c "mysql -u root -e \"GRANT USAGE ON *.* TO 'chronic'@'%' IDENTIFIED BY 'Test321';\""
-lxc exec Database -- sh -c "mysql -u root -e \"GRANT ALL privileges ON MyDB.* TO 'chronic'@'%';\""
+lxc exec Database -- sh -c "mysql -u root -e \"CREATE USER 'cloud_user' IDENTIFIED BY 'Test321';\""
+lxc exec Database -- sh -c "mysql -u root -e \"GRANT USAGE ON *.* TO 'cloud_user'@'%' IDENTIFIED BY 'Test321';\""
+lxc exec Database -- sh -c "mysql -u root -e \"GRANT ALL privileges ON MyDB.* TO 'cloud_user'@'%';\""
 lxc exec Database -- sh -c "mysql -u root -e \"SET PASSWORD FOR 'root'@'localhost' = PASSWORD('321TestForRoot321');\""
 lxc exec Database -- sh -c "mysql -u root -e \"FLUSH PRIVILEGES;\""
